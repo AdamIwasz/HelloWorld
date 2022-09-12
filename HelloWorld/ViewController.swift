be75fe8a0a3314d120c7013bbeb20eec58077815
adaiwa <adam.iwaszkiewiczai@gmail.com>
2022-09-12 17:47:12 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Adam Iwaszkiewicz UCTI on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showMessage(sender: UIButton){
+        let alertController = UIAlertController(title: "Welcome to My First App",
+                                                message: "Hello World",
+                                                preferredStyle: UIAlertController.Style.alert)
+        alertController
+    }
+    
+}
+
